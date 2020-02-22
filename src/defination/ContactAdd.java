ac83bf0f09e680ec15e43f99d6211075dd7eb52b
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2020-02-22 05:34:30 -0800

    add logic to get details from user of the contact and add it to conbook

--- src/defination/ContactAdd.java
+++ src/defination/ContactAdd.java
@@ -1,5 +1,30 @@
 package defination;
 
-public class ContactAdd {
+import java.util.ArrayList;
 
+public class ContactAdd {
+    public ContactAdd() {
+    }
+
+    public boolean addContact(String fname, String lname, String MobNo, String Mob2, String Email) {
+        ArrayList contact;
+        if (Mob2 != null) {
+            contact = new ArrayList();
+            contact.add(fname);
+            contact.add(lname);
+            contact.add(MobNo);
+            contact.add(Mob2);
+            contact.add(Email);
+            Database.setConBook(contact);
+        } else {
+            contact = new ArrayList();
+            contact.add(fname);
+            contact.add(lname);
+            contact.add(MobNo);
+            contact.add(Email);
+            Database.setConBook(contact);
+        }
+
+        return true;
+    }
 }
--- src/defination/Database.java
+++ src/defination/Database.java
@@ -1,5 +1,20 @@
 package defination;
 
-public class Database<E> {
+import java.util.ArrayList;
 
+public class Database<E> {
+    private static ArrayList conBook = new ArrayList<>();
+
+    public static ArrayList getConBook() {
+        return conBook;
+    }
+
+    public static void setConBook(ArrayList conBook) {
+        Database.conBook.add(conBook);
+    }
+
+    public static void removecontact(int index) {
+        Database.conBook.remove(index);
+    }
 }
+
