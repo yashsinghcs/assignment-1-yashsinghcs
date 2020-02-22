c772d62f4045c04bdb4430342be07fac7565e8bc
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2020-02-22 05:42:50 -0800

    the program is a bit different from what required but performs all the operation required

---
+++ src/defination/ContactSearch.java
@@ -0,0 +1,40 @@
+package defination;
+
+import adt.ContactSearchADT;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ContactSearch<E> implements ContactSearchADT {
+    @Override
+    public void searchContact(String Name) {
+        ArrayList contacts = new ArrayList(Database.getConBook());
+        for (int i = 0; i < contacts.size(); i++) {
+            ArrayList contact = new ArrayList((Collection) contacts.get(i));
+            if ((contact.get(0) + " " + contact.get(1)).equals(Name)) {
+
+                printSearchedContact((ArrayList) contacts.get(i));
+            }
+        }
+    }
+
+    @Override
+    public void printSearchedContact(ArrayList contact) {
+        if (contact.size() == 5) {
+            System.out.println("--------------------------------------------");
+            System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+            System.out.println("Mobile no(s)=" + contact.get(2) + "," + contact.get(3));
+            System.out.println("Email id=" + contact.get(4) + "\n");
+            System.out.println("--------------------------------------------");
+
+        } else {
+            System.out.println("--------------------------------------------");
+            System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+            System.out.println("Mobile no=" + contact.get(2));
+            System.out.println("Email id=" + contact.get(3) + "\n");
+            System.out.println("--------------------------------------------");
+
+        }
+
+    }
+}
