404bfa2449ab57d67d2b39638443484f3c7cf038
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2020-02-22 05:40:47 -0800

    add logic to print the contact list but the list is not sorted yet

---
+++ src/defination/ContactView.java
@@ -0,0 +1,43 @@
+package defination;
+
+import adt.ContactViewADT;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ContactView<E> implements ContactViewADT {
+
+
+    @Override
+    public void printAllContact() {
+
+        ArrayList contacts = new ArrayList(Database.getConBook());
+        for (int i = 0; i < contacts.size(); i++) {
+            ArrayList contact = new ArrayList((Collection) contacts.get(i));
+            if (contact.size() == 5) {
+                System.out.println("--------------------------------------------");
+                System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+                System.out.println("Mobile no(s)=" + contact.get(2) + "," + contact.get(3));
+                System.out.println("Email id=" + contact.get(4) + "\n");
+                System.out.println("--------------------------------------------");
+
+            } else {
+                System.out.println("--------------------------------------------");
+                System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+                System.out.println("Mobile no=" + contact.get(2));
+                System.out.println("Email id=" + contact.get(3) + "\n");
+                System.out.println("--------------------------------------------");
+
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public E printAllContactactSort() {
+        return null;
+    }
+
+}
