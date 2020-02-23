0363db1758191cfc2cd03b4640555eae48ecc7c5
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2020-02-23 20:36:14 +0530

    added logic to print the names avalible in ContactDelete so user can delete it just typing the full name

--- src/defination/ContactDelete.java
+++ src/defination/ContactDelete.java
@@ -4,11 +4,23 @@ import adt.ContactDeleteADT;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class ContactDelete<E> implements ContactDeleteADT {
     @Override
     public boolean deleteContact(String name) {
+        System.out.println("here are all the names of your contact sorted");
+        System.out.println("----------------------------------------------------------");
+        ArrayList contactNames = new ArrayList();
         ArrayList contacts = new ArrayList(Database.getConBook());
+        for (int i = 0; i < contacts.size(); i++) {
+            ArrayList contact = new ArrayList((Collection) contacts.get(i));
+            contactNames.add(contact.get(0) + " " + contact.get(1));
+        }
+        Collections.sort(contactNames, String.CASE_INSENSITIVE_ORDER);
+        for (int i = 0; i < contactNames.size(); i++) {
+            System.out.println(contactNames.get(i).toString());
+        }
         for (int i = 0; i < contacts.size(); i++) {
             ArrayList contact = new ArrayList((Collection) contacts.get(i));
             if ((contact.get(0) + " " + contact.get(1)).equals(name)) {
