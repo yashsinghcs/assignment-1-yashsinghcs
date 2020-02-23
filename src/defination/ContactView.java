07feb298c17b07cb7216e4d175a220bb8a5d6cff
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2020-02-23 20:29:48 +0530

    add new logic to print data in sorted form but previous output also there

--- src/defination/ContactView.java
+++ src/defination/ContactView.java
@@ -19,7 +19,6 @@ public class ContactView<E> implements ContactViewADT {
         for (int i = 0; i < contacts.size(); i++) {
             ArrayList contact = new ArrayList((Collection) contacts.get(i));
             contactNames.add(contact.get(0) + " " + contact.get(1));
-
         }
         Collections.sort(contactNames, String.CASE_INSENSITIVE_ORDER);
         for (int i = 0; i < contactNames.size(); i++) {
@@ -49,6 +48,36 @@ public class ContactView<E> implements ContactViewADT {
                 System.out.println("Email id=" + contact.get(3) + "\n");
                 System.out.println("--------------------------------------------");
 
+            }
+
+        }
+        for (Object name : contactNames) {
+
+            for (int i = 0; i < contacts.size(); i++) {
+                ArrayList contact = new ArrayList((Collection) contacts.get(i));
+                if (name.equals(contact.get(0) + " " + contact.get(1))) {
+                    if (contact.size() == 5) {
+                        System.out.println("--------------------------------------------");
+                        System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+                        System.out.print("Mobile no(s)=");
+                        for (int k = 2; k < contact.size() - 1; k++) {
+
+                            System.out.print(contact.get(k) + ",");
+                        }
+                        System.out.println("");
+                        System.out.println("Email id=" + contact.get(contact.size() - 1) + "\n");
+                        System.out.println("--------------------------------------------");
+
+                    } else {
+                        System.out.println("--------------------------------------------");
+                        System.out.println("Name=" + contact.get(0) + " " + contact.get(1));
+                        System.out.println("Mobile no=" + contact.get(2));
+                        System.out.println("Email id=" + contact.get(3) + "\n");
+                        System.out.println("--------------------------------------------");
+
+                    }
+
+                }
             }
 
         }
